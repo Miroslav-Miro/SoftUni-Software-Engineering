a0e28b0154c6f3336b7c083b943d0800ea546e74
Miroslav-Miro <miroslavmazalov@gmail.com>
2021-02-06 02:47:58 -0100

    Add projects

---
+++ First steps in programing/05.GreetingbyName/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _05.GreetingbyName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}!");
+
+
+        }
+    }
+}
