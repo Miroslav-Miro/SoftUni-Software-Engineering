9a1ad3b9d64e4e3b13e7e240d5a0ec73a96f2ee9
Miroslav-Miro <miroslavmazalov@gmail.com>
2021-02-06 02:59:10 -0100

    Format

---
+++ First steps in programing/02.Nums1.10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2.Nums1._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            for (; i <= 10; i = i + 1 ) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
