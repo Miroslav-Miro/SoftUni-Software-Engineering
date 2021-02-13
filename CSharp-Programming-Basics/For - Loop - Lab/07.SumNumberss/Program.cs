2411e1d3b8f9908cfcecc76cbd869271e7d83ee0
Miroslav-Miro <miroslavmazalov@gmail.com>
2021-02-13 02:49:18 -0100

    Change folder name

---
+++ CSharp-Programming-Basics/For - Loop - Lab/07.SumNumberss/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.SumNumberss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int number = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num= int.Parse(Console.ReadLine());
+                number += num;
+            }
+            Console.WriteLine(number);
+        }
+    }
+}
