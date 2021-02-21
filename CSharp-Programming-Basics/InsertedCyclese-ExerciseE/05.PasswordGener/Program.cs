fa27e82eb44b1bbbcd54792fe755d3195d85a44e
Miroslav-Miro <miroslavmazalov@gmail.com>
2021-02-21 01:38:35 -0100

    Change names

---
+++ CSharp-Programming-Basics/InsertedCyclese-ExerciseE/05.PasswordGener/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _05.PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int j = int.Parse(Console.ReadLine());
+            int c2 = 0;
+            int countC = 0;
+            int countD = 0;
+           
+            
+            //char c1 = Convert.ToChar(c);
+            //Console.WriteLine($"{a}{b}{c1}");
+            for (int a = 1; a < n; a++)
+            {
+                for (int b = 1; b < n; b++)
+                {
+                    for (int c = 'a'; c <= 'z'; c++)
+                    {
+                        countC++;
+                        if (countC <= 1)
+                        {
+                            c2 = c + j -1 ;
+                        }
+                        
+                        if (c > c2)
+                        {
+                            break;
+                        }
+                        char c1 = Convert.ToChar(c);
+                        for (int d = 'a'; d <= 'z'; d++)
+                        {
+                            countD++;
+                            if (countD <= 1)
+                            {
+                                c2 = d + j -1 ;
+                            }
+
+                            
+                            if (d > c2)
+                            {
+                                break;
+                            }
+                            char d1 = Convert.ToChar(d);
+                            for (int e = 1; e < n; e++)
+                            {
+                                if (e+1 > a && e+1 > b)
+                                {
+                                    Console.Write($"{a}{b}{c1}{d1}{e + 1}");
+                                    Console.Write(" ");
+                                }
+                                
+                            }
+                            //Console.Write(" ");
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ CSharp-Programming-Basics/InsertedCyclese-ExerciseE/05.PasswordGenerator1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _05.PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int j = int.Parse(Console.ReadLine());
+            int c2 = 0;
+            int count = 0;
+            int count1 = 0;
+            //char c1 = Convert.ToChar(c);
+            //Console.WriteLine($"{a}{b}{c1}");
+            for (int a = 1; a < n; a++)
+            {
+                for (int b = 1; b < n; b++)
+                {
+                    for (int c = 'a'; c < 'z'; c++)
+                    {
+                        count++;
+                        //c2 = c - 100;
+                        //j = j - j;
+                        
+                        if (count > 1)
+                        {
+
+                        }
+                        else
+                        {
+                            c2 = Math.Abs(c - 100);
+                        }
+                        if (c2 > j)
+                        {
+                            break;
+                        }
+                        char c1 = Convert.ToChar(c);
+                        for (int d = 'a'; d < 'z'; d++)
+                        {
+                            count1++;
+                            //c2 = d - 100;
+                            
+                            // c2 = Math.Abs(c - 100);
+                            if (count1 > 1)
+                            {
+
+                            }
+                            else
+                            {
+                                c2 = Math.Abs(d - 100);
+                            }
+                            if (c2 > j)
+                            {
+                                break;
+                            }
+                            char d1 = Convert.ToChar(d);
+                            for (int e = 1; e < n ; e++)
+                            {
+                                Console.WriteLine($"{a}{b}{c1}{d1}{e+1}");
+                            }
+
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
