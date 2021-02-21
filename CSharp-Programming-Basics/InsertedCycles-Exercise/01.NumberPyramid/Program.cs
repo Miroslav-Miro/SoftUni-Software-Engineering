fefd8aaae558748f6d286fb808c45d865b253564
Miroslav-Miro <miroslavmazalov@gmail.com>
2021-02-21 01:39:42 -0100

    Add files

---
+++ CSharp-Programming-Basics/InsertedCycles-Exercise/01.NumberPyramid/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01.NumberPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int rows = 1; rows <= n; rows++) 
+            {
+                
+                for (int i = 0; i < rows; i++)
+                {
+                    count++;
+                    Console.Write($"{count} ");
+                    if (count == n)
+                    {
+                        break;
+                    }
+                }
+                if (count == n)
+                {
+                    break;
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
